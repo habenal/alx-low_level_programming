793d9b49b8ff0bd8a63a2de8f6aa5929c75e35ca
habenal <habenalem09@gmail.com>
2023-07-14 12:54:18 -0700

    fix 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,18 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Haben Alem
+ */
+
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
