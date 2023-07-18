a6da2054331a27a8464c8b7c3afb7d28b839e744
habenal <habenalem09@gmail.com>
2023-07-18 11:31:23 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
 *print_alphabet-printstheenglishalphabetfroma-z.
 *Return:Nothing.
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,13 @@
-#include"holberton.h"
+#include "main.h"
 /**
-*main-checkalphabet.
-*passparameterintc
-*
-*Return:Always0.
+*_isalpha - controls if a character is alphabetical
+*@c: character to be verified
+*Return: return 0 or 1
 */
-int_isalpha(intc)
+int _isalpha(int c)
 {
-    if(( c>= 'a' && c <= 'z') || ( c>= 'A' && c <= 'Z'))
-        return (1);
-    else
-        return (0);
-}
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
 
+return (0);
+}
